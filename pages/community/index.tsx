b84f49c49a39de7dde251d2b1721e4829c112ee2
Sheeyx <sheeyx00@gmail.com>
2024-06-06 01:43:37 +0900

    feat: organize and test routing system

---
+++ pages/community/index.tsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+const Community = () => {
+  return (
+    <div>
+        COMMUNITY{""}
+        <button onClick={()=>alert("Hello MIT")} style={{margin: "15px"}}>
+            PressMe
+        </button>
+    </div>
+  )
+}
+
+export default Community;
---
+++ pages/property/index.tsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+const Property = () => {
+  return (
+    <div>
+        PROPERTY LIST
+    </div>
+  )
+}
+
+export default Property;
