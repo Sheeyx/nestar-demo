ab4988e44dfa716dc5382dd03ad6e73962b36f31
Sheeyx <sheeyx00@gmail.com>
2024-06-06 02:22:11 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme"
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("App COMPONENT - PAGES ROUTER");
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
   
-  return <Component {...pageProps} />;
+  // Socket.io, Redux, MUI ....
+  return (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>);
 }
